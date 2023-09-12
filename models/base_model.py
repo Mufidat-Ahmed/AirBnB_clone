b993a5eb603a3639d0ab68854f8d9272cddeb1c2
IdaraKoffi <idarakoffi@gmail.com>
2023-09-12 11:36:19 -0700

    updated

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,19 @@ class BaseModel:
             *args: Arguments
             **kwargs: keyword arguments
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        storage.new(self)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            storage.new(self)
+        else:
+            f = "%Y-%m-%dT%H:%M:%S.%f"
+            for key, value in kwargs.items():
+                if key == 'created_at' or key == 'updated_at':
+                    value = datetime.strptime(kwargs[key], f)
+                if key != '__class__':
+                    setattr(self, key, value)
+
 
     def save(self):
         """updated_at with the current datetime"""
@@ -23,16 +32,26 @@ class BaseModel:
         storage.save()
 
     def __str__(self):
-        """Returns a string rep of basemodel"""
-        classname = self.__class__.__name__
-        return "[{}], ({}), {}".format(classname, self.id, self.__dict__)
+        """returns class name, id and attribute dictionary
+        """
+        class_name = "[" + self.__class__.__name__ + "]"
+        dct = {k: v for (k, v) in self.__dict__.items() if (not v) is False}
+        return class_name + " (" + self.id + ") " + str(dct)
 
     def to_dict(self):
-        """ converts basemodel instance to dictionary
-        Return: dictionary containing all keys/values
+        """to create a new dictionary, adding a key and returning
+        datemtimes converted to strings
         """
-        my_dict = self.__dict__.copy()
-        my_dict['__class__'] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        new_dict = {}
+
+        for key, values in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = values.strftime("%Y-%m-%dT%H:%M:%S.%f")
+            else:
+                if not values:
+                    pass
+                else:
+                    new_dict[key] = values
+        new_dict['__class__'] = self.__class__.__name__
+
+        return new_dict
